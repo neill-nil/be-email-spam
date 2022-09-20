a2830539a8a01048a67afeb3d21e81b46045bb54
Neill Rawani <neill.rawani@hexaviewtech.com>
2022-09-20 17:57:16 +0530

    Changed databases and apis, added change password

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import configparser
 import re
 import joblib
-from model import Emails, Users, Drafts, db, login
+from model import Emails, Users, Senders, Receivers, Drafts, db, login
 from services.prediction import predict
 
 from flask import Flask, request, jsonify, render_template, redirect, session, url_for
@@ -8,15 +9,25 @@ from flask_login import login_required, current_user, login_user,logout_user
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_restful import Resource, Api
+from flask_wtf.csrf import CSRFProtect
 # from wtforms import StringField, PasswordField, BooleanField, IntegerField, validators
 # from wtforms.validators import InputRequired, Email, Length
+
+import numpy
+from psycopg2.extensions import register_adapter, AsIs
+def addapt_numpy_float64(numpy_float64):
+    return AsIs(numpy_float64)
+def addapt_numpy_int64(numpy_int64):
+    return AsIs(numpy_int64)
+register_adapter(numpy.float64, addapt_numpy_float64)
+register_adapter(numpy.int64, addapt_numpy_int64)
 
 app = Flask(__name__)
 
 
 
 app.config["SECRET_KEY"] = "mysecretkey"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Neillkira99-_-@localhost/data"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 
@@ -34,6 +45,9 @@ login.login_view= 'login'
 @app.before_first_request
 def create_all():
     db.create_all()
+
+# csrf = CSRFProtect()
+# csrf.init_app(app)
 
 migrate = Migrate(app, db)
 
@@ -78,6 +92,7 @@ def register():
             data = {'note': 'This Mobile number has already been registered..'}
             return jsonify(data), 401
         user=Users(fname=first_name, lname=last_name, useremail=email, phno=contact_no)
+        print("***********************&*&&* IDHAR", user)
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
@@ -99,18 +114,24 @@ def logout():
 
 #for both primary and spam
 class Inbox(Resource):
+    # @login_required
     def get(self, folder, id):
-            # mails = Emails.query.filter((Emails.receiver_id==current_user.id)).first()
-        mails = db.session.query(Emails, Users).filter((Emails.receiver_id==id) & (Emails.folder==folder) & (Emails.is_deleted.is_(False))).join(Users,Emails.sender_id==Users.id).all()
+        
+        mails = db.session.query(Emails, Receivers, Users).filter\
+        ((Emails.id==Receivers.email_id) & (Receivers.receiver_id==id) & (Receivers.folder==folder) &\
+        (Emails.sender_id==Users.id) & (Receivers.is_deleted.is_(False))).all()  
+
         records = []
-        for a, b  in mails:
-            ic_map = a.json()
-            ic_map.update(b.json())
+        for a, b, c  in mails:
+            ic_map = a.to_json()   
+            ic_map.update(b.to_json())
+            ic_map.update(c.to_json())
             records.append(ic_map)
 
         return records
     
 class Update(Resource):
+    @login_required
     def put(self):
         email_id = request.json["email_id"]
         mail = Emails.query.get(email_id)
@@ -140,49 +161,72 @@ class Update(Resource):
 
     
 class Compose(Resource):
+    # @login_required
     def post(self):
         text = request.json['email_body']
         subject = request.json['subject']
-        user_id = request.json['userId']
-        r_emid = request.json['to']
-        # exists = bool(Users.query.filter_by(useremail=r_emid).first())
-        if Users.query.filter_by(useremail=r_emid).first():
-            r_id = db.session.query(Users.id).filter_by(useremail=r_emid)
-        else:
-            msg = {'note': 'This email does not exist'}
-            return msg, 404
+        user_id = request.json['userId'] # sender (id)
+        r_emids = request.json['to'] # list of receivers emails
+        r_ids = []
+        for r_emid in r_emids:
+            print("********************************", r_emid)
+            if Users.query.filter_by(useremail=r_emid).first():
+                r_id = db.session.query(Users.id).filter_by(useremail=r_emid).scalar()
+                print("********************************", type(r_id))
+                r_ids.append(r_id)
+            else:
+                msg = {'note': 'This email does not exist'}
+                return msg, 404
         comb_text = subject + " " + text
         pred = predict(comb_text, loaded_rfc)
-        mail = Emails(email_text=text, subject=subject, receiver_id=r_id, sender_id=user_id, prediction=pred, folder='spam' if pred==1 else 'primary')
-        db.session.add(mail)
+        print("********************************", r_ids)
+        mail = Emails(email_text=text, subject=subject, receivers=r_ids, sender_id=user_id, prediction=pred)
+        db.session.add(mail) 
+        db.session.commit()
+        for r_id in r_ids:
+            r_pref = Receivers(receiver_id = r_id, email_id = mail.id, folder='spam' if pred==1 else 'primary')
+            s_pref = Senders(sender_id = user_id, email_id = mail.id)
+        db.session.add(r_pref)
+        db.session.add(s_pref)
         db.session.commit()
 
         return {'note': 'mail sent!'}
 
 class Starred(Resource):
+    @login_required
     def get(self, id):
-        mails = db.session.query(Emails, Users).filter((Emails.receiver_id==id) & (Emails.star_marked.is_(True))).join(Users,Emails.sender_id==Users.id).all()
+
+        mails = db.session.query(Emails, Receivers, Users).filter\
+        ((Emails.id==Receivers.email_id) & (Receivers.receiver_id==id) & \
+        (Receivers.star_marked.is_(True)) & (Emails.sender_id==Users.id)).all()
+        
         records = []
-        for a, b  in mails:
-            ic_map = a.json()   
-            ic_map.update(b.json())
+        for a, b, c  in mails:
+            ic_map = a.to_json()   
+            ic_map.update(b.to_json())
+            ic_map.update(c.to_json())
             records.append(ic_map)
 
         return records
 
 
 class Sent(Resource):
+    #@login_required
     def get(self, id):
-        mails = db.session.query(Emails, Users).filter(Emails.sender_id==id).join(Users,Emails.receiver_id==Users.id).all()
+        mails = db.session.query(Emails, Senders, Users).filter\
+        ((Senders.sender_id==id) & (Emails.id==Senders.email_id)).all()
+
         records = []
-        for a, b  in mails:
-            ic_map = a.json()
-            ic_map.update(b.json())
+        for a, b, c  in mails:
+            ic_map = a.to_json()   
+            ic_map.update(b.to_json())
+            ic_map.update(c.to_json())
             records.append(ic_map)
 
         return records
 
 class Trash(Resource):
+    #@login_required
     def get(self, id):
         mails = db.session.query(Emails, Users).filter((Emails.receiver_id==id) & (Emails.is_deleted.is_(True))).join(Users,Emails.sender_id==Users.id).all()
         records = []
@@ -192,6 +236,41 @@ class Trash(Resource):
             records.append(ic_map)
 
         return records
+
+class ChangePass(Resource):
+    #@login_required
+    def put(self, id):
+        user=Users.query.filter_by(id=id).first()
+        if user.check_password(request.json["password"]):
+            new_pass = request.json["new_pass"]
+            confirm_new_pass = request.json["confirm_new_pass"]
+            if new_pass == confirm_new_pass:
+                user.set_password(new_pass)
+                db.session.commit()
+                return {'note': 'Password has been updated'}
+            else:
+                data = {'note': "Passwords do not match"}
+                return data, 401
+        else:
+            data = {'note': "Incorrect password"}
+            return data, 401
+
+class ForgotPass(Resource):
+    def put(self):
+        email = request.json['email']
+        ans1 = request.json['ans1']
+        ans2 = request.json['ans2']
+        user = Users.query.filter_by(useremail=email).first()
+        if ans1==user.verifyans1 and ans2==user.verifyans2:
+            new_pass = request.json["new_pass"]
+            confirm_new_pass = request.json["confirm_new_pass"]
+            if new_pass == confirm_new_pass:
+                user.set_password(new_pass)
+                db.session.commit()
+                return {'note': 'Password has been updated'}
+            else:
+                data = {'note': "Passwords do not match"}
+                return data, 401
 
     # def put(self, id):
     #     email_id = request.json["email_id"]
@@ -207,6 +286,8 @@ api.add_resource(Starred, '/inbox/starred/<int:id>')
 api.add_resource(Update, '/update')
 api.add_resource(Trash, '/inbox/trash/<int:id>')
 api.add_resource(Sent, '/inbox/sent/<int:id>')
+api.add_resource(ChangePass, '/changepass/<int:id>')
+api.add_resource(ForgotPass, '/forgotpass')
 
     
 
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import enum
 from typing import Text
 from xmlrpc.client import Boolean
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, ForeignKey, Integer, Table, String, JSON, DateTime, BOOLEAN, Enum
+from sqlalchemy import Column, ForeignKey, Integer, Table, String, JSON, DateTime, BOOLEAN, Enum, Numeric
 from sqlalchemy.orm import declarative_base, relationship
 from flask_login import login_required, current_user, login_user,logout_user, LoginManager, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -27,7 +27,7 @@ class Users(db.Model, UserMixin):
     useremail = Column('User Email', String(60), unique=True)
     fname = Column('First name', String(50))
     lname = Column('Last name', String(50))
-    phno = Column('Phone Number', Integer(), unique=True)
+    phno = Column('Phone Number', Numeric, unique=True)
     password_hash = Column('Password', String(250))
     created_at = Column(DateTime(timezone=True), default=datetime.utcnow())
     updated_at = Column(DateTime(timezone=True), default=datetime.utcnow(), onupdate=datetime.utcnow())
@@ -39,7 +39,7 @@ class Users(db.Model, UserMixin):
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
     
-    def json(self):
+    def to_json(self):
         return {'email': self.useremail, 'firstname': self.fname, 'lastname': self.lname}
 
        
@@ -47,6 +47,42 @@ class MyEnum(enum.Enum):
     spam = 1
     primary = 0
 
+
+class Senders(db.Model):
+    __tablename__ = 'senders'
+    
+    '''
+    sender_id: email
+    sender_email: emailID of the sender
+    receiver_email: emailID of the sender
+    '''
+    id = Column(Integer, primary_key=True)
+    sender_id = Column(Integer, ForeignKey('users.id'))
+    email_id = Column(Integer, ForeignKey('emails.id'))
+    star_marked = Column(BOOLEAN, default=False)
+    is_deleted = Column(BOOLEAN, default=False)  
+
+    def to_json(self):
+        return {'deleted': self.is_deleted, 'starred': self.star_marked}
+
+
+class Receivers(db.Model):
+    __tablename__ = 'receivers'
+    
+    '''
+    sender_id: email
+    sender_email: emailID of the sender
+    '''
+    id = Column(Integer, primary_key=True)
+    receiver_id = Column(Integer, ForeignKey('users.id'))
+    email_id = Column(Integer, ForeignKey('emails.id'))
+    star_marked = Column(BOOLEAN, default=False) 
+    is_deleted = Column(BOOLEAN, default=False)  
+    is_read = Column(BOOLEAN, default=False)
+    folder = Column(Enum(MyEnum), nullable=False, default='primary')
+
+    def to_json(self):
+        return {'read': self.is_read, 'deleted': self.is_deleted, 'starred': self.star_marked}
 
 class Emails(db.Model):
     __tablename__ = 'emails'
@@ -61,20 +97,17 @@ class Emails(db.Model):
     '''
     id = Column('id',Integer, primary_key=True)
     sender_id = Column(Integer, ForeignKey('users.id'))
-    receiver_id = Column(Integer, ForeignKey('users.id'))
+    receivers = Column(JSON) # list of receivers emails
+    cc = Column(JSON) # list of cc emails
     subject = Column(String(200))
     email_text = Column(String(1000), unique=False, nullable=False)
     prediction = Column(Integer, nullable=False)
-    folder = Column(Enum(MyEnum), nullable=False, default='primary')
     attachment = Column(JSON, nullable=True)
     created_at = Column(DateTime(timezone=True), default=datetime.utcnow())
     updated_at = Column(DateTime(timezone=True), default=datetime.utcnow(), onupdate=datetime.utcnow())
-    star_marked = Column(BOOLEAN, default=False)
-    is_deleted = Column(BOOLEAN, default=False)
-    is_read = Column(BOOLEAN, default=False)
 
-    def json(self):
-        return {'email_id': self.id, 'email_body': self.email_text, 'subject': self.subject, 'read': self.is_read, 'deleted': self.is_deleted, 'starred': self.star_marked}
+    def to_json(self):
+        return {'email_id': self.id, 'email_body': self.email_text, 'subject': self.subject, 'receivers': self.receivers}
 
 
 
